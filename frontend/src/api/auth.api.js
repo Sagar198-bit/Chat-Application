04b11adc7842121ee8627672bbee297ce8274bc8
Sagar Rawat <sagarrawat2845@gmail.com>
2026-08-15 12:13:35 +0530

    fixed the api changes

--- frontend/src/api/auth.api.js
+++ frontend/src/api/auth.api.js
@@ -1,5 +1,5 @@
 import {API} from "../api/axios.instances.js"
 
-export const Signup = (data) => API.post("auth/signup", data).then((res) => res);
-export const Login = (data) => API.post("auth/login", data).then((res) => res);
-export const getme = () => API.get("auth/me").then((res) => res);
+export const Signup = (data) => API.post("/auth/signup", data).then((res) => res);
+export const Login = (data) => API.post("/auth/login", data).then((res) => res);
+export const getme = () => API.get("/auth/me").then((res) => res);
